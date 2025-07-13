8e9d477be5249192dfca6892777ecf545ec5e8ec
manojmanu27-dell <138210050+manojmanu27-dell@users.noreply.github.com>
2025-07-13 12:11:31 +0000

    Initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Header } from '../layout/header/header';
+import { Sidebar } from '../layout/sidebar/sidebar';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Header,Sidebar  ],
+  templateUrl: './app.html',
+  styleUrl: './app.scss',
+})
+export class App {
+
+}
