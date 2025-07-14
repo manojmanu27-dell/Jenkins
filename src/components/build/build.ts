234f91baec01144359afed7a7f29c6a968932d86
manojmanu27-dell <138210050+manojmanu27-dell@users.noreply.github.com>
2025-07-14 05:33:10 +0000

    final commit

--- src/components/build/build.ts
+++ src/components/build/build.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule} from '@angular/forms';
 import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+
 @Component({
   selector: 'app-build',
   imports: [
-    MatSelectModule,FormsModule, ReactiveFormsModule
+    MatSelectModule,FormsModule,MatInputModule,MatButtonModule
     ],
   templateUrl: './build.html',
   styleUrl: './build.scss'
 })
 export class Build {
- 
+ public readonly script = "inserIntoDynamoDB.js";
+ public readonly region = "us-east-1";
+ public readonly env = "dit";
+
+ public navigateToHome(){
+      const host = window.location.origin;
+    window.open(host + "/#/home", "_self");
+    window.location.reload();
+ }
 }
--- src/components/home/home.ts
+++ src/components/home/home.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatProgressBarModule, CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
-export class Home {
+export class Home implements OnInit {
+  showProgressBar = true;
+ngOnInit():void{
+setTimeout(()=>{
+  this.showProgressBar = false;
+},20000)
+}
 
 }
